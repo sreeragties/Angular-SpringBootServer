c03346f67ab85ee719c513012ea7adb8455da61e
Sreerag <sreeragts2105@gmail.com>
2021-09-22 10:20:48 +0530

    hello

--- src/main/java/net/javaguides/springboot/Application.java
+++ src/main/java/net/javaguides/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "https://boiling-island-04165.herokuapp.com/")
+
 @SpringBootApplication
 public class Application {
 
